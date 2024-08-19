3c3683f0a83ea0cff009e3c8b5f1d384b4caab41
aisling <aisling.hemlock@gmail.com>
2024-08-19 22:00:58 +0100

    chore: Add lazy loading for images in ProjectSummary component

--- src/components/ProjectSummary,.tsx
+++ src/components/ProjectSummary,.tsx
@@ -1,12 +1,14 @@
+import { Suspense } from "react";
 import { SummaryLayout } from "./layouts/SummaryLayout";
 import { SummaryFilterNav } from "./navbars/SummaryFilterNav";
 
 export default function ProjectSummary() {
 	function SummaryLayoutFallback() {
-		return <h3>loading summaries</h3>}
+		return <h3>loading summaries</h3>;
+	}
 	return (
-		<>
+		<Suspense fallback={<SummaryLayoutFallback />}>
 			<SummaryLayout />
-		</>
+		</Suspense>
 	);
 }
